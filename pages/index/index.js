088c45e536612c12fc9600c14a280dd9aeac0d29
knkp1209 <30972393+knkp1209@users.noreply.github.com>
2017-08-23 15:12:08 +0800

    v0.1
    v0.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,57 @@
 //获取应用实例
 const app = getApp()
 
+Page({
+  data: {
+    sg: null,
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    cat: [
+
+    ]
+  },
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  onLoad: function () {
+  var that = this//不要漏了这句，很重要
+  wx.request({
+  url: 'https://weapp.chengshiyunguanjia.com/Platform/API/getAppAdList?rid=100124',
+  headers: {
+  'Content-Type': 'application/json'
+  },
+  success: function (res) {
+  //将获取到的json数据，存在名字叫zhihu的这个数组中
+   that.setData({
+   sg: res.data.result,
+   //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+   })
+  }
+ })
+ }
+})
+
+
+
 // Page({
 //   data: {
 //     motto: 'Hello World',
